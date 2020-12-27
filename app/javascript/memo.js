6a5ba698af091b87a118ca4c85ab1ba5b9e272fb
kedani <75819419+kedani@users.noreply.github.com>
2020-12-27 21:34:02 +0900

    keda

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,6 +1,7 @@
 function memo() {
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
+    
     const formData = new FormData(document.getElementById("form"));
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
